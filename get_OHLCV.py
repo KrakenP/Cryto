548a0449714917d66d73201ebf99ffb85d4a9a9e
piven <lhpiven@gmail.com>
2023-04-04 16:37:53 +0800

    Modified the OHLCV to get any time range value

--- get_OHLCV.py
+++ get_OHLCV.py
@@ -23,26 +23,26 @@ stop_loss_pct = 0.05  # 5%
 
 
 def OHLCV(exchange, symbol, timeframe, start_date=None):
-    start_timestamp = int(start_date.timestamp() * 1000)
     today = datetime.today()
     length = (today - start_date).days
-    ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since=start_timestamp, limit=1000)
-    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    df.set_index('timestamp', inplace=True)
-    if length > 1000:
-        another = today - timedelta(days=length-1000)
-        another_stamp = int(another.timestamp() * 1000)
-        new = exchange.fetch_ohlcv(symbol, timeframe, since=another_stamp, limit=1000)
-        new_df = pd.DataFrame(new, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-        new_df['timestamp'] = pd.to_datetime(new_df['timestamp'], unit='ms')
-        new_df.set_index('timestamp', inplace=True)
-        final_df = pd.concat([df, new_df])
+    df_list = []
+    s = start_date
+    while length > 0:
+        start_timestamp = int(s.timestamp() * 1000)
+        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since=start_timestamp, limit=min(1000, length + 1))
+        df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+        df.set_index('timestamp', inplace=True)
+        df_list.append(df)
+        length -= 1000
+        s += timedelta(days=1000)
+    if len(df_list) > 1:
+        final_df_r = pd.concat(df_list)
     else:
-        final_df = df
-    return final_df
+        final_df_r = df_list[0]
+    return final_df_r
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     start_date = datetime.strptime('2020-01-01', '%Y-%m-%d')
     final_df = OHLCV(exchange, symbol, timeframe, start_date)
